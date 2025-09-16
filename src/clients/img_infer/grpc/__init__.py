de0963051f0fbb0d642d797a51f6409f60a8b440
nasyuu <beaufreehugs@gmail.com>
2025-09-16 22:31:31 +0800

    1

--- src/clients/img_infer/grpc/__init__.py
+++
@@ -1,1 +0,0 @@
-from .standard import GrpcConfig, create_stub, infer_dir, infer_image
--- src/core/labelme/__init__.py
+++
@@ -1,6 +0,0 @@
-from .converter import (
-    ConvertConfig,
-    batch_convert_dir,
-    build_image_index,
-    convert_one_file,
-)

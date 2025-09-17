f1f72367ca111289fa44d4c736231660a9317f21
nasyuu <beaufreehugs@gmail.com>
2025-09-17 23:11:41 +0800

    1

--- client/image_infer/grpc/grpc_module_pb2.py
+++ client/image_infer/grpc/grpc_module_pb2.py
@@ -4,37 +4,34 @@
 # source: grpc_module.proto
 # Protobuf Python Version: 6.31.1
 """Generated protocol buffer code."""
+
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
+
 _runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.PUBLIC,
-    6,
-    31,
-    1,
-    '',
-    'grpc_module.proto'
+    _runtime_version.Domain.PUBLIC, 6, 31, 1, "", "grpc_module.proto"
 )
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
 
 
-
-
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x11grpc_module.proto\"2\n\x07Request\x12\x13\n\x0bstream_name\x18\x01 \x01(\t\x12\x12\n\nimage_data\x18\x02 \x01(\x0c\")\n\x08Response\x12\x0c\n\x04\x63ode\x18\x01 \x01(\x05\x12\x0f\n\x07message\x18\x02 \x01(\t2=\n\x13StandardGrpcService\x12&\n\rstandardInfer\x12\x08.Request\x1a\t.Response\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
+    b'\n\x11grpc_module.proto"2\n\x07Request\x12\x13\n\x0bstream_name\x18\x01 \x01(\t\x12\x12\n\nimage_data\x18\x02 \x01(\x0c")\n\x08Response\x12\x0c\n\x04\x63ode\x18\x01 \x01(\x05\x12\x0f\n\x07message\x18\x02 \x01(\t2=\n\x13StandardGrpcService\x12&\n\rstandardInfer\x12\x08.Request\x1a\t.Response"\x00\x62\x06proto3'
+)
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'grpc_module_pb2', _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "grpc_module_pb2", _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
-  DESCRIPTOR._loaded_options = None
-  _globals['_REQUEST']._serialized_start=21
-  _globals['_REQUEST']._serialized_end=71
-  _globals['_RESPONSE']._serialized_start=73
-  _globals['_RESPONSE']._serialized_end=114
-  _globals['_STANDARDGRPCSERVICE']._serialized_start=116
-  _globals['_STANDARDGRPCSERVICE']._serialized_end=177
+    DESCRIPTOR._loaded_options = None
+    _globals["_REQUEST"]._serialized_start = 21
+    _globals["_REQUEST"]._serialized_end = 71
+    _globals["_RESPONSE"]._serialized_start = 73
+    _globals["_RESPONSE"]._serialized_end = 114
+    _globals["_STANDARDGRPCSERVICE"]._serialized_start = 116
+    _globals["_STANDARDGRPCSERVICE"]._serialized_end = 177
 # @@protoc_insertion_point(module_scope)
